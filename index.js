e2f5b1d29f65ab49670dd330a9dd938439da3c19
Melisa Quesada <melisa.quc@gmail.com>
2019-07-09 13:30:34 -0500

    adds multer to handle form data

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/', function (req, res) {
+app.post('/', upload.none(), function (req, res) {
     console.log(req.body),
     res.send('Got a POST request');
   });
