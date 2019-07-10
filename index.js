10ef39009c380e2b958c579e2dab969f5bf75f55
Melisa Quesada <melisa.quc@gmail.com>
2019-07-10 10:35:12 -0500

    changes port

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.post('/', upload.none(), function (req, res) {
     res.send('Got a POST request');
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log('listening on port 3001');
 });
